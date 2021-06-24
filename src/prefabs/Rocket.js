63f0a6122fc3aa98bda9a85373e0e2fd31c7fe52
Jordan Fickel <jfickel@ucsc.edu>
2021-06-24 16:03:56 -0700

    collision added

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,5 +5,35 @@ class Rocket extends Phaser.GameObjects.Sprite{
         //add to scene
 
         scene.add.existing(this);
+        this.isFiring = false; //is it already going?
+        this.moveSpeed = 2; //pixels per frame
+    }
+
+    update(){
+        if(!this.isFiring){
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width){
+                this.x -= this.moveSpeed;
+
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - (borderUISize + this.width)){
+                this.x += this.moveSpeed;
+
+            }
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(keyF)){
+            this.isFiring = true;
+        }
+        if(this.isFiring && this.y >= borderUISize*3 + borderPadding){
+            this.y -= this.moveSpeed;
+        }
+        //reset on miss
+        if(this.y <= borderUISize*3 + borderPadding){
+            this.reset();
+        }
+    }
+
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - (borderUISize + borderPadding);
     }
 }
