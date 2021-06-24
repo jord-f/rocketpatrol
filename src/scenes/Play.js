b4174ead378e6e729df0dfacc99b3c965a791ad2
Jordan Fickel <jfickel@ucsc.edu>
2021-06-24 16:43:12 -0700

    added timer and end screen

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -45,6 +45,27 @@ class Play extends Phaser.Scene {
                             frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first: 0}), 
                             frameRate:30});
 
+
+        this.p1Score = 0;
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color:'#843605',
+            align:'right',
+            padding: {top: 5, bottom: 5},
+            fixedWidth: 100
+        }
+
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+        //60 second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to RESTART', scoreConfig).setOrigin(0.5);
+        }, null, this);
+
     }
 
     update(){
@@ -89,7 +110,11 @@ class Play extends Phaser.Scene {
             ship.reset();
             ship.alpha = 1;
             boom.destroy();
-        })
+        });
+
+        //add score and repaint score display
+        this.p1Score +=ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 
 }
