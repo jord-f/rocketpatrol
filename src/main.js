b15e77ba7a267a41b18c418b04828f06ffdc9791
Jordan Fickel <jfickel@ucsc.edu>
2021-06-28 21:17:08 -0700

    extra rocket control

--- src/main.js
+++ src/main.js
@@ -25,3 +25,4 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 // Randomize each spaceship's movement direction at the start of each play      5
 // parallax scrolling -                                                         10
 // particle emitter and particle explosion -                                    20
+// Allow the player to control the Rocket after it's fired                      5
